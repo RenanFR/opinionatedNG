08d1b34d96a275a944fb0a40d33280e587d78721
renan.rodrigues <renan.rodrigues@accountfy.com>
2019-06-13 21:51:42 -0300

    first commit

---
+++ src/app/layout/recent‐threads/recent.threads.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'opinionated-recent-threads',
+    templateUrl: './recent.threads.component.html'
+})
+export class RecentThreadsComponent {
+
+}
